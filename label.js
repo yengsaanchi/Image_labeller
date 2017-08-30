5f8395d362a6e7128b66bca259ebb26363d488b7
yengsaanchi <ysaanch@ncsu.edu>
2017-08-30 18:10:15 -0400

    updated

--- label.js
+++ label.js
@@ -1,7 +1,5 @@
 var counter = -1;
-var imageUrls =  ['http://emojipedia-us.s3.amazonaws.com/cache/8b/bd/8bbd3405b0a197214e229428c23dbe60.png', 'http://emojipedia-us.s3.amazonaws.com/cache/05/d1/05d1ba284ee1a3bfe4e0f68988baafb9.png', 
-     'http://emojipedia-us.s3.amazonaws.com/cache/99/4c/994c5997c7a509703cc53ec2000bb258.png', 'http://emojipedia-us.s3.amazonaws.com/cache/59/0c/590c832e73a472c416bf9d8bfdd02a4a.png'];
-     
+
 var images =  ['images/p4.JPEG', 'images/p5.JPEG', 'images/p6.JPEG', 'images/p7.JPEG', 'images/lake.jpg'];
 
  //function to fetch urls of images based on indices entered into "From" and "To" which are then stored in array called srcArray 
